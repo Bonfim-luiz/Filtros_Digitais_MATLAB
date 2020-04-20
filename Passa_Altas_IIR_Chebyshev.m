0ee5a80dbce9d5dac89b384081dc5429d41bc3ae
Luiz Bonfim <63732228+Bonfim-luiz@users.noreply.github.com>
2020-04-20 10:01:29 -0300

    Update and rename Passa_Altas_Chebyshev.m to Passa_Altas_IIR_Chebyshev.m

--- Passa_Altas_IIR_Chebyshev.m
+++ Passa_Altas_IIR_Chebyshev.m
@@ -1,5 +1,8 @@
 % Highpass IIR, com fc = 2500 Hz, banda de passagem f ≥ 2500 Hz.
 % Aproximação Chebyshev
+% Frequência de amostragem seja de fa = 44100 Hz.
+% Plot da resposta em frequência em Magnitude (dB x frequência normalizada)
+% e a resposta de fase (fase em radianos x frequência normalizada).
 
 [N,Wn] = cheb1ord(0.18,0.18,0.5,30,'s');
 [B,A] = cheby1(4,0.5,Wn,'s');
